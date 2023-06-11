092a38eb2f118ca2fb75d56ecf41ea2802eb3b3b
@OneJuicyDoor <shephardcam@gmail.com>
2023-06-11 11:42:02 -0300

    created the timer code

---
+++ timer1.js
@@ -0,0 +1,15 @@
+let durations = process.argv.slice(2);
+//Loop through each number provided
+for (let duration of durations) {
+  
+  let seconds = Number(duration);  //Make each imput a number
+ 
+  if (typeof seconds !== 'number' || isNaN(seconds) || seconds < 0) { //check if each imput is actually a valid number, if not log that its not a number
+    console.log("not a valid number");
+  } else {
+    setTimeout(() => { //beep after the correct amount of time (also say it beeped cause the sound seems to have trouble playing in succsion)
+      console.log("it beeped");
+      process.stdout.write('\x07');
+    }, seconds * 1000);
+  }
+}
