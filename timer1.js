7638fbe898a3f46645eca4bb8ca7203dfe59590e
Cameron Shephard <127575295+OneJuicyDoor@users.noreply.github.com>
2023-06-12 01:38:07 -0300

    moved comments

--- timer1.js
+++ timer1.js
@@ -1,10 +1,10 @@
 let durations = process.argv.slice(2);
 //Loop through each number provided
 for (let duration of durations) {
-  
-  let seconds = Number(duration);  //Make each imput a number
- 
-  if (typeof seconds !== 'number' || isNaN(seconds) || seconds < 0) { //check if each imput is actually a valid number, if not log that its not a number
+   //Make each imput a number
+  let seconds = Number(duration); 
+ //check if each imput is actually a valid number, if not log that its not a number
+  if (typeof seconds !== 'number' || isNaN(seconds) || seconds < 0) { 
     console.log("not a valid number");
   } else {
     setTimeout(() => { //beep after the correct amount of time (also say it beeped cause the sound seems to have trouble playing in succsion)
